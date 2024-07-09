ee62709efb3a5b944274bf46eeb01c1bd9ce8b26
Pavel Koprov <pkoprov@yandex.ru>
2024-07-10 02:57:43 +0300

    Сделал неограниченную историю просмотров без повторений

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -3,25 +3,86 @@ package service;
 import dto.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final ArrayList<Task> taskHistory = new ArrayList<>();
+    private final HashMap<Integer, Node> nodeMap = new HashMap<>();
+
+    private Node head;
+    private Node tail;
 
     public void add(Task task) {
+        remove(task.getTaskId());
+        Node newNode = linkLast(task);
+        nodeMap.put(task.getTaskId(), newNode);
 
-        if (task == null) {
+    }
+
+    private Node linkLast(Task task) {
+        Node newNode = new Node();
+
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+            newNode.prev = null;
+            newNode.next = null;
+        } else {
+            newNode.prev = tail;
+            newNode.data = task;
+            newNode.next = null;
+
+            newNode.prev.next = newNode;
+            this.tail = newNode;
+        }
+        return newNode;
+    }
+
+    public void remove(int id) {
+        Node node = nodeMap.remove(id);
+
+        if (node == null) {
             return;
         }
-        if (taskHistory.size() == 10) {
-            taskHistory.remove(0);
-        }
 
-        Task copyTask = new Task(task.getTaskName(), task.getTaskDescription(), task.getTaskId(), task.getTaskStatus());
-        taskHistory.add(copyTask);
+        removeNode(node);
+    }
+
+    private void removeNode(Node node) {
+        if (node.prev == null && node.next == null) {
+            this.head = null;
+            this.tail = null;
+
+        } else if (node.prev == null) {
+            node.next.prev = null;
+            this.head = node;
+        } else if (node.next == null) {
+            node.prev.next = null;
+            this.tail = node;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
     }
 
     @Override
     public ArrayList<Task> getHistory() {
-        return new ArrayList<>(taskHistory);
+        ArrayList<Task> taskHistory = new ArrayList<>();
+        if (head == null) {
+            return taskHistory;
+        }
+        taskHistory.add(head.data);
+        Node nextNode = head.next;
+
+        while(nextNode != null) {
+            taskHistory.add(nextNode.data);
+        }
+        return taskHistory;
+    }
+
+    private static class Node {
+        Node prev;
+        Node next;
+        Task data;
     }
 }
